d39a2bea19fa90d07cecfc7a43645aad6d277d3c
5ard33n <fardeennaseem2006@gmail.com>
2024-11-08 17:38:36 +0530

    Update script.js

---
+++ 03_Digital_clock/script.js
@@ -0,0 +1,12 @@
+const clock = document.querySelector('.clock')
+
+
+setInterval(function () {
+let date = new Date();
+clock.innerHTML = date.toLocaleTimeString();
+// console.log(date.toLocaleTimeString());
+}, 1000)    //1000 millisecons = 1sec
+
+
+
+
